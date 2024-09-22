597fbc8c0b5548e2751a32429cfc758b4666356e
Shashank.V <113120781+ShashankVadde@users.noreply.github.com>
2024-09-21 20:42:35 -0500

    Add files via upload

---
+++ Apple Pie/Apple Pie/Game.swift
@@ -0,0 +1,38 @@
+//
+//  Game.swift
+//  Apple Pie
+//
+//  Created by Vadde, Shashank (Student) on 9/21/24.
+//
+
+import Foundation
+
+struct Game {
+    var word: String
+    var incorrectMovesRemaining: Int
+    var guessedLetters: [Character]
+    
+    var formattedWord: String {
+        var guessedWord = ""
+        for letter in word {
+            if guessedLetters.contains(letter) {
+                guessedWord += "\(letter)"
+            } else {
+                guessedWord += "_"
+            }
+        }
+        return guessedWord
+    }
+    
+    mutating func playerGuessed(letter: Character) {
+        guessedLetters.append(letter)
+        if !word.contains(letter) {
+            incorrectMovesRemaining -= 1
+        }
+    }
+}
+
+
+
+
+
